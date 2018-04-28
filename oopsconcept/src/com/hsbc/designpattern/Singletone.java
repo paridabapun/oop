d081800719480e100c02590493044e2bf280f1f4
Bapun <Bapun@Bapun-PC>
2018-04-28 21:26:45 +0530

    Using Public constructor

--- oopsconcept/src/com/hsbc/designpattern/Singletone.java
+++ oopsconcept/src/com/hsbc/designpattern/Singletone.java
@@ -2,22 +2,32 @@ package com.hsbc.designpattern;
 
 import java.io.Serializable;
 
+import com.hsbc.praveen.Example1;
+
 public class Singletone implements Serializable {
-	
-// Using Approach-1
+
+	// Using Approach-1
 
 	private static Singletone single_instance = null;
 
-	private Singletone() {
-		System.out.println("New Object Created ");
-	}
-
-	public static Singletone getInstance() {
+	/*
+	 * private Singletone() { System.out.println("New Object Created "); }
+	 */
+	public static Singletone getInstance() throws Exception {
 		if (null == single_instance) {
 			single_instance = new Singletone();
 		}
 
 		return single_instance;
+	}
+
+	// Using Approach-2 using public constructor
+
+	public Singletone() throws Exception {
+		if (null != single_instance) {
+			throw new Exception("Object Already Exist");
+		}
+
 	}
 
 }
--- oopsconcept/src/com/hsbc/designpattern/TestDesinPattern.java
+++ oopsconcept/src/com/hsbc/designpattern/TestDesinPattern.java
@@ -1,20 +1,21 @@
 package com.hsbc.designpattern;
 
 public class TestDesinPattern {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
+		Singletone singletone2 = Singletone.getInstance();
+		Singletone singletone3 = Singletone.getInstance();
+		Singletone singletone4 = Singletone.getInstance();
+		Singletone singletone5 = Singletone.getInstance();
+		Singletone singletone6=new Singletone();
 		
 		
-
-		Singletone singletone2=Singletone.getInstance();
-		Singletone singletone3=Singletone.getInstance();
-		Singletone singletone4=Singletone.getInstance();
-		Singletone singletone5=Singletone.getInstance();
 		System.out.println(singletone2);
 		System.out.println(singletone3);
 		System.out.println(singletone4);
 		System.out.println(singletone5);
-		
+		System.out.println(singletone6);
+
 	}
 
 }
