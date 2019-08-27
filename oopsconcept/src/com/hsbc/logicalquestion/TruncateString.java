ba29607978f9bdf44ab0652136cba90f7d5832f1
Bhaggu <paridabapun@gmail.com>
2019-08-28 00:22:42 +0530

    Added one Utility class

---
+++ oopsconcept/src/com/hsbc/logicalquestion/TruncateString.java
@@ -0,0 +1,36 @@
+package com.hsbc.logicalquestion;
+
+import java.util.Arrays;
+
+public class TruncateString {
+
+
+    /*
+     * Method use to truncate a specific string to certain length
+     * */
+    public static String truncateString(String name) {
+
+        if (null != name &&  name.length() > 42) {
+
+            name = name.substring(0, 42);
+
+
+        }
+        return name;
+    }
+
+    public static void main(String[] args) {
+
+        String name = "My Name is Bhagabata Parida , working a softwer Developer in HSBC";
+        System.out.println("Original String " + name);
+
+        String truncateName = truncateString(name);
+
+        System.out.println("Truncate String " + truncateName);
+
+        String name1 = "";
+        name1 = name1.length() > 22 ? name1.substring(0, 22) : name1;
+        System.out.println(name1);
+
+    }
+}
