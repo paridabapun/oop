728f57ac0434d09142db1a793bbe4c32bfc5d02c
Bhaggu <paridabapun@gmail.com>
2020-01-02 22:09:00 +0530

    Introduction to java 8

--- oopsconcept/src/com/hsbc/javaeight/HelloWorld.java
+++ oopsconcept/src/com/hsbc/javaeight/HelloWorld.java
@@ -3,10 +3,37 @@ package com.hsbc.javaeight;
 import com.hsbc.serialization.S;
 
 public class HelloWorld {
+
+    public void m1(){
+        System.out.println("Inside m1()");
+    }
+
     public static void main(String[] args) {
 
+//        WITHOUT JAVA 8
+        Drawable drawable = new Drawable() {
+            @Override
+            public void draw() {
+                System.out.println("Default Draw");
+            }
+        };
+        drawable.draw();
+//        WITH JAVA 8
+        Drawable drawable1 = () -> {
+            System.out.println("In lamda drawable");
+        };
+        drawable1.draw();
 
-        System.out.println("HelloWorld");
+        HelloInteface  helloInteface = () -> {
+            System.out.println("Hello");
+        };
+        helloInteface.hellow();
+
+        HelloInteface  helloInteface1 = () -> { System.out.println("Hello helloInteface1"); };
+        helloInteface1.hellow();
+        HelloInteface  helloInteface2 = () ->  System.out.println("Hello helloInteface2");;
+        helloInteface2.hellow();
     }
+
 
 }
