129a35b679bb09786f1fd9da803c2e7a5e05b92e
Bhaggu <paridabapun@gmail.com>
2020-01-02 21:43:13 +0530

    Introduction to java 8

---
+++ oopsconcept/src/com/hsbc/javaeight/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.hsbc.javaeight;
+
+import com.hsbc.serialization.S;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+
+        System.out.println("HelloWorld");
+    }
+
+}
