ec4171972a19f4d98c4d0576e5b6310e99c0268d
Bapun <paridabapun@gmail.com>
2021-03-24 23:51:04 +0530

    My First Commits (Liza)

---
+++ oopsconcept/src/com/hsbc/liza/HelloJava.java
@@ -0,0 +1,12 @@
+package com.hsbc.liza;
+
+import java.util.Arrays;
+
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("I love you my janu ... love please leran java quickly from your woodby ");
+    }
+
+
+}
